3cbe299a77aaa57f043b6c8e725ccec273ae5fcd
ToniTepes <antoniostacy@gmail.com>
2023-03-10 17:39:12 -0500

    toni page issue

--- src/content_option.js
+++ src/content_option.js
@@ -23,49 +23,7 @@ const dataabout = {
 // Writing Pages
 const toni = {
     title: "Toni Y. Josepph",
-    tonijoseph: `“‘Get busy, nigger’ I was told. ‘This here is one lazy niggeress’” are the first words I ever read by my aunt Toni. They are from a book composed of her articles and essays from over her years as a journalist. Even though the book was dedicated to me, I could never get past the first page. I don’t exactly know why, and I don’t exactly remember the moment I read on from there, but it was shamefully recent. The book was published in 1994 right after her death, but for whatever reason I never read past those few words. For decades I carried around a copy of One Voice with me constantly. It lived on a bookshelf in whatever dump I was occupying at the time and survives to this day not ten feet from where I’m sitting currently. This copy is special. It was sent to me from Nancy, my aunt's best friend, when I was a little kid. It may or may not have been a sign or an urge from Nancy to read my auntie’s words one day and feel the righteous calling of truth that Toni felt. Choosing to dedicate the book to me may or may not have been a message from my aunt to do the same. Either way, now I have read the book and it changed everything. Now it's time for me to follow a path that I wanted to be on before my eye opening moment. A path I may or may not have been set on.
-
-
-
-    \n\ \n\ My Aunt Toni, who is my namesake, was born and raised in Cleveland. When she was younger she displayed gifted qualities early on and was sent to study at prestigious private schools and institutions. She attended Alexander Hamilton in junior highschool, after that she was accepted into Glen Oak School for Girls, which is now Gilmour Academy. Following high school she started off her post-secondary career at Vassar College, which was just the beginning. She continued her higher education attending Iowa Writer Workshop’s creative writing program followed by attending Columbia Journalism School. Throughout her time in academia she spent much time in rooms full of people who did not look like her, and she mentioned this often. I know how I felt when I started attending suburban schools with higher numbers of whites. Although in my case, they were middle/lower middle class whites, which in my experience are easier to get along with. I, however, could not imagine what it was like being a Black woman-a Black child nonetheless in her situation, as she was merely a teenager when her journey started. In context to the rest of the strong black women in our family, it is not entirely surprising though. They are all exceptionally intelligent, adaptable, fierce and compassionate. For all that, she remained diligent, constantly being among the white wealthy class, in white wealthy institutions, all while being a “‘down’ sister from Cleveland.” - Donna Britt. Being able to walk through these spaces was a worthy endeavor that served her well in her professional career. Because, afterall, where she was headed after that wasn’t going to be any less white.
-    
-    
-    
-    Aunt Toni was an exceptional writer. I only recently found out that she had a passion for fiction writing as well, and obviously she was great at it. However, her career was rooted in journalism. I have become so entranced with her essays and op-eds the past couple of years. Her interviews and profiles have an elaborate range of prominent predominantly black activists, artist, advocates and visionaries. Over the years I have found countless articles and images depicting her connections with people such as Toni Morrison, Kwame Ture, Cornel West, James Baldwin, Keith Haring, Alice Walker and many other favored social heroes. Funny, I have an original Keith Haring that belonged to her. I actually had no idea what it was until a close friend came over, for the first time, and shouted “Ni**a is this an original Keith Haring!?” I just loved the image, the “Free South Africa,'' message on the bottom of it and that it belonged to my Aunt. Her crowning achievement was working at the Dallas Morning News “invoking the spirit and power of the black experience,” which won her a Pulitzer Prize. At this point, and even before, Auntie had her pick of where she was going with her writing. I am certain that people repeatedly ask “what do you/what do you want to write?” I do not have a good answer for that just yet because I really want to write about everything. Well, Tony did just that, and now I know I can as well.
-    
-    
-    
-    As I mentioned before, my aunt shared tables with many prominent voices but she was not just a vessel to translate the gospels of civil rights warriors. She also involved herself and lived alongside the experiences she was writing about. May 5th, 1990, she published an essay titled My Life as a Slave in which she wrote, “Few things make me as mad as the N-word. I never say it, and I challenge other African Americans who do.” while experiencing a day in the life of a slave workshop that she asked to join. Something thought-provoking for me, because I do use the N-word. An article entitled 500 Americans in Paris toured an artist conference that took place in France over a week. The main focus of this article is to highlight the successes of Blacks in Paris, speaking of discussions she had with artists and their enthusiasm for Black art, specifically African American. Separate from this, I have always wanted to visit and live in France. This article and another made it certain, for me, that Paris is where I want to eventually end up. For her admissions letter to Columbia she elegantly described the culture shock she experienced at Gilmore. Saying, “I learned something there about the different cultures and religions which taught me acceptance rather than tolerance.” Another line that speaks to me and my exposure to many different cultures. I could only hope to someday reach her level of acceptance.
-    
-    
-    
-    “If you like truth, people, writing, you most certainly need to pursue training in journalism” Which are words of hers that I read for the first time four days ago, well after I made the decision to take my writing career more seriously. A long time friend of my Aunt Toni sent me books every year and I took advantage of that for so long. So long that I never really read the inscriptions she would write. My books are all in storage as I write this, along with all the articles that I have that my aunt wrote. But I did not store my aunt's book. I never do and I have been carrying around my personal copy of it for decades. I recently read the inscription while searching for some kind of message in my aunt’s writing, as I always do. Written by Nancy, the most impactful line, to me is “This book is for you, if anyone.” A literal message, that I finally saw. My Aunt saw something in me that she relayed to her closest friends and confidants, and I feel that she did leave me clues. Clues and people to connect with in order to give me some direction for when I was ready to step into my gift. A gift, that I have been told by so many over the years, that I need to find and step into. I am now ready to step into my gift and to see the truth in the world and in myself. Toni died when I was about eight years old, and oddly enough, I feel like she knew me better than anyone else in my life ever really could. She saw a path for me that no one in my family, except Grandma, could even entertain as a possibility. For that, all the clues, and all the resources I have no excuse not to follow, she is my guiding light. I, at very least, owe her an honest try at what she saw me accomplishing in my future, and now I am trying as hard as I can. 
-    
-    
-    
-    In finding myself through Aunt Toni’s work, I have found so much more acceptance in the person that I am. I have seen that the societal thoughts, comparisons, triumphs and failures that I have committed to truths were and are, in fact, a piece of her within me. Even before I knew my aunt's thoughts, a fire was lit inside of me from the pilot light of her soul. I have remained honest and humble though myself and the tribulations of my life. I do not think I’m special but feel that: 
-    
-    
-    
-    “I’ve highlighted here those incidents that surfaced in memory when I attempted to define my life, the events, conversations and observations that remained in my subconscious, those that have affected or will affect my character and will have some bearing on my personal and professional development. They are physically intangible, but in no way remote. I haven’t had epiphanies on beaches or mountaintops, but discovered instead that although these fragments do not constitute autobiography, I would call them an impression, all that I can offer.”
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    Joseph, T. Y. (1994). My Life as a Slave. In One voice: A Pulitzer prize-winning journalist tells stories about the black experience (p. 3). essay, Dallas-Fort Worth Association of Black Communicators.
-    
-    Britt, D. (1993, July 27). On screen, no justice for the sisters. The Washington Post. Retrieved May 2, 2022, from https://www.washingtonpost.com/archive/local/1993/07/27/on-screen-no-justice-for-the- sisters/e1557c91-b020-480c-a893-aebac21ea6c9/
-    
-    Joseph, T. Y. (1985, January 1). Admissions Essay A. Columbia University Graduate School of Journalism
-    
-    Joseph, T. Y. (1985, January 1). Admissions Essay B. Columbia University Graduate School of Journalism
-    
-    `,
+    tonijoseph: `yo wtf, whay arent you changin?`,
 };
 
 const depression = {
