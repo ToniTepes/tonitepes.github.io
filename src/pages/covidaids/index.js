6ead60588ca4b5a2c628d056a371e002dd30fbe5
ToniTepes <antoniostacy@gmail.com>
2023-05-24 14:25:45 -0400

    added 404

--- src/pages/covidaids/index.js
+++ src/pages/covidaids/index.js
@@ -13,12 +13,12 @@ export const CovidAndAids = () => {
             <Container className="About-header">
                 <Helmet>
                     <meta charSet="utf-8" />
-                    <title> Covidaids | {meta.title}</title>
+                    <title> Covid and AIDs | {meta.title}</title>
                     <meta name="description" content={meta.description} />
                 </Helmet>
                 <Row className="mb-5 mt-3 pt-md-3">
                     <Col lg="8">
-                        <h1 className="display-4 mb-4">Covidaids</h1>
+                        <h1 className="display-4 mb-4">Covid and AIDS</h1>
                         <hr className="t_border my-4 ml-0 text-left" />
                     </Col>
                 </Row>
